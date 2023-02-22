43bf8fb9b7942ef777bf7ad89ef900bf1e81f5d5
alanalzi <117884402+alanalzi@users.noreply.github.com>
2023-02-22 18:46:19 +0700

    Add files via upload

---
+++ 1_D_71220824.py
@@ -0,0 +1,2 @@
+cuhu = float(input("Masukan nilai curah hujan masbroo: "))
+c = ("Cuaca terang masbro"if cuhu == 0  else print("Curah hujan ringan masbro")if cuhu >= 0.5 and cuhu <=20 else print(" Curah hujan sedang masbro")if cuhu >= 21 and cuhu <= 50 else print("Curah hujan lebat masbro")if cuhu >=51 and cuhu <= 100 else print("Curah hujan ekstrem masbro")if cuhu >100 else print()) 
---
+++ 4_D_71220824.py
@@ -0,0 +1,29 @@
+nam = input("Masukan Nama Lengkap Anda: ")
+pro = input("Masukan Prodi Anda: ")
+nil = input("Masukan Nilai (dalam huruf) yang Anda Dapat: ")
+try:   
+    nil = int(nil)
+    print("inputan masbro ga valid")
+except:
+    if nil == 'A':
+        print("Nilai masbro 4.00, Mantap kali masbro satu ini")
+    elif nil == 'A-':
+        print("Nilai masbro 3.75, kamu keren!")
+    elif nil == 'B+':
+        print("Nilai masbro 3.25, Lumayan bang")
+    elif nil == 'B':
+        print("Nilai masbro 3.0, ya masi okelah masbro")
+    elif nil == 'B-':
+        print("Nilai masbro 2.75, ayo masbro belajar lagi")
+    elif nil == 'C+':
+        print("Nilai masbro 2.25, okela bang masih lulus")
+    elif nil == 'C':
+        print("Nilai masbro 2.0, ada masalah apa masbro")
+    elif nil == 'D':
+        print("Nilai masbro 1.0, wadoo massbro kenapa ini")
+    elif nil == 'E':
+        print("Nilai masbro 0, sudah tidak tertolong")
+    else:
+        print("inputan masbro ga valid")
+
+    
